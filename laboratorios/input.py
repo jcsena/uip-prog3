0f1fc95c606229ff21b8e9c33e79e3ff13dc42f1
Julio Sena <julio.657.js@gmail.com>
2015-09-21 21:42:45 -0500

    se aplica el pythonlint para depuracion de codigo

--- laboratorios/input.py
+++ laboratorios/input.py
@@ -1,38 +1,39 @@
 
 '''
 calcular el area y perimetro de un rectangulo dada la base y la altura
-de 5 cm y 7 cm respectivamente ademas debe convertir los resultados del area y perimetro a metros y pulgadas
+de 5 cm y 7 cm respectivamente ademas debe convertir los
+resultados del area y perimetro a metros y pulgadas
 '''
 
-#se definen las variables de base y altura
-base = 5;
-altura = 7;
+'''se definen las variables de base y altura'''
+base = 5
+altura = 7
 
-#se calcula el area
-area = base * altura;
+# se calcula el area
+area = base * altura
 
-#se calcula perimetro
+# se calcula perimetro
 perimetro = (base * 2) + (altura * 2)
 
-#se converiter el area a metros
-areaToMetro  = area/100
+# se converiter el area a metros
+areaToMetro = area/100
 
-#se convierte el area a pulgadas
+# se convierte el area a pulgadas
 areaToPulaga = area/2.54
 
-#se convierte el perimetro a metros
+# se convierte el perimetro a metros
 perimetroToMetro = perimetro/100
 
-#se convierte el perimetro a pulgadas
+# se convierte el perimetro a pulgadas
 perimetroToPulaga = perimetro/2.54
 
 
-# se imprimen los resultados
+# se imprimen los resultados del area
 print("area en centimetros = " + str(area) + " cm")
-print("perimetro en centimetros = " + str(perimetro) + " cm")
-
 print("area a metros = " + str(areaToMetro) + " m")
 print("area a pulgadas = " + str(areaToPulaga) + " pul")
 
-print("perimetro en metros" + str(perimetroToMetro) + " m")
-print("perimetro en pulgadas" + str(perimetroToPulaga) + " pul")
+# se imprimen los resultados del perimetro
+print("perimetro en centimetros = " + str(perimetro) + " cm")
+print("perimetro en metros = " + str(perimetroToMetro) + " m")
+print("perimetro en pulgadas = " + str(perimetroToPulaga) + " pul")
